985ac163b02a0641e6f41bb2778a8e6e88fc7f06
rubyGoldberg <z5162022@cse.unsw.edu.au>
2018-11-14 09:46:15 +1100

    Scraping the Web

--- tags.pl
+++ tags.pl
@@ -2,11 +2,22 @@
 use warnings; 
 use strict; 
 
-# given the URL of a web page fetches it by running wget and prints the HTML tags it uses.
+# Given the URL of a web page fetches it by running wget and prints the HTML tags it uses.
 # The tag should be converted to lower case and printed in sorted order with a count of how often each is used.
 
 # Don't count closing tags.
 
+if (@ARGV != 1) { 
+	die "usage: $0 <url>\n";
+}
 
+my $url = shift @ARGV;
 
+open my $FH, "-|", "wget -q -O- $url", or die "Could not scrape $url\n"; 
+my @contents = <$FH>;
 
+close $FH;
+
+print @contents;
+
+my @tags; # an array of tags, with 1 opening tag each
