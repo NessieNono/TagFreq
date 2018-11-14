f0e016f0ee74e4e8f914bf6bf1b9b14cb23fa4ff
rubyGoldberg <z5162022@cse.unsw.edu.au>
2018-11-14 13:37:55 +1100

    Finished

--- tags.pl
+++ tags.pl
@@ -1,63 +1,55 @@
 #!/usr/bin/perl -w
 use warnings; 
-use strict; 
 
 # Given the URL of a web page fetches it by running wget and prints the HTML tags it uses.
 # The tag should be converted to lower case and printed in sorted order with a count of how often each is used.
 
 # Don't count closing tags.
 
-if (@ARGV != 1) { 
-	die "usage: $0 <url>\n";
+if (@ARGV != 2) { 
+	die "usage: $0 <-l/-w> <url>\n";
 }
 
-my $url = shift @ARGV;
-my @contents;
-open my $FH, "-|", "wget -q -O- $url", or die "Could not scrape $url\n"; 
-# my @contents = <$FH>;
-while (my $line = <$FH>) { 
-	$line =~ s/<!--.*?-->//g; # remove comments
-	if ($line =~ m/\S/) { # only save non-empty lines
-		$line = lc $line; # all to lowercase
-		$line =~ s/^\s+//; # delete leading spaces
-		push @contents, $line;
-	}
+my $flag = shift @ARGV; 
+my $file = shift @ARGV;
+
+if ($flag eq "-l") { 
+	open $FH, "<", "$file" or die "Could not open file $file\n";
+} elsif ($flag eq "-w") {
+	open $FH, "-|", "wget -q -O- $file", or die "Could not scrape $file\n"; 
+} else { 
+	die "invalid flag.\n";
 }
+
+my $content = join("", <$FH>); # entire content is read into a string
 close $FH;
 
-my @tags; # an array of tags, with 1 opening tag each
-foreach my $line (@contents) { 
-	# save the line into my array of tags if: 
-	# non greedy match of < something > 
-	my @tags_in_line = grep(/<.*?>/, $line);
-	print "these are my tags in line: @tags_in_line\n";
-	push @tags, @tags_in_line;
+# clean the concent; 
+$content =~ tr/A-Z/a-z/; # to lowercase
+$content =~ s/<!--.*-->//g; # remove contents
+
+# collect all the different tags
+my @tags = $content =~ /<\s*(\w+)/g;
+
+# count the frequency of each tag 
+my %hash; 
+
+# count the tags
+my $size = 0; 
+foreach my $tag (@tags) { 
+	$hash{$tag}++;
+	$size++; 
 }
-
-print @tags;
-# at this stage, the tags may still be of the form: 
-# word < div id=""> word
-# lets clean the filth 
-
-# my @clean_tags; 
-# foreach my $line (@tags) { 
-# 	print "this is a line containing a single tag: $line\n";
-# }
-
-
-# # now only save all the lines which have beginning tags
-# my @beginning_tags; 
-# foreach my $line (@tags) { 
-# 	if ($line =~ m/<\//) { 
-# 		print "this is and ending tag: $line\n";
-# 	} else { 
-# 		push @beginning_tags, $line;
-# 	}
-# }
-
-# print "beginning tags!"; 
-# print @beginning_tags;
-
-
-
-
+my $types = keys %hash;
+print "Reading $file\n";
+print "$types types of tags found\n";
+print "$size total tags found.\n";
+print "--------------------------\n";
+print "       tag:  freq  percent\n";
+print "--------------------------\n";
+foreach my $key (sort {$hash{$b} <=> $hash{$a}} keys %hash) { 
+	# a percent sign is escaped by doubling it 
+	my $ratio = $hash{$key} * 100 / $size;
+	my $bracketed_key = "<".$key.">";
+	printf "%10s: %5d %5.1f%%\n", $bracketed_key, $hash{$key}, $ratio;
+}
